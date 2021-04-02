c6c76c25b236ca42db6e5e9545237ba9e8afe4ff
Ashwinigm <ash.gm16@gmail.com>
2021-04-02 22:42:47 +0530

    Room confimation redirected to room.html

--- application/searchapp/urls.py
+++ application/searchapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import HomePageView, SearchResultsView, add_room, SearchByGenreView, SearchByNameView, SearchByGenre, SearchByName, add_confirmation
+from .views import HomePageView, SearchResultsView, add_room, SearchByGenreView, SearchByNameView, SearchByGenre, SearchByName, add_confirmation, submitpage
 
 urlpatterns = [
     path('search/', SearchResultsView.as_view(), name='search_results'),
@@ -9,6 +9,7 @@ urlpatterns = [
     path('searchgenreview/', SearchByGenreView.as_view(), name='searchbygenre_view'),
     path('searchnameview/', SearchByNameView.as_view(), name='searchbyname_view'),
     path('add/', add_room, name="add_room"),
+    path('room/', submitpage, name="room"),
     path('addconfirmation/', add_confirmation.as_view(), name="add_confirmation"),
     path('', HomePageView.as_view(), name='home'),
     #path('', showform().as_view(), name='home'),
