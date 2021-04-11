14b2d1d3b58c8b9282304e67337512bb072fa5cd
Bryan Fetner <bfetner@mail.sfsu.edu>
2021-04-10 23:13:21 -0700

    Added room jxs

---
+++ application/frontend/src/components/Room.jsx
@@ -0,0 +1,12 @@
+import React, { Component, useState } from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+import Axios from "axios";
+const Room = () => {
+  return (
+    <div>
+      <div>Room</div>
+    </div>
+  );
+};
+
+export default Room;
