a142d3181c50d531345f13e058bd7f8d5aa21df5
Bryan Fetner <bfetner@mail.sfsu.edu>
2021-04-10 18:46:59 -0700

    Added the nav bar and some routes in app.js

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
+import Addroom from "./components/Addroom";
+import NavBar from "./components/navbar";
 import axios from "axios";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
 
 const App = () => {
-  return(
+  return (
     <div>
       <BrowserRouter>
+        <NavBar />
         <Switch>
-          <Route path='/' exact component={Modal}></Route>
+          <Route path="/" exact component={Modal}></Route>
+          <Route path="/Addroom" exact component={Addroom}></Route>
         </Switch>
       </BrowserRouter>
     </div>
-  )
-}
+  );
+};
 
 export default App;
