8364bb88d6d3c7d452ed3245033921dfa005952b
Bryan Fetner <bfetner@mail.sfsu.edu>
2021-04-10 23:09:41 -0700

    Updated nav bar and Main page

--- application/frontend/src/components/navbar.jsx
+++ application/frontend/src/components/navbar.jsx
@@ -1,54 +1,25 @@
-import React, { Component } from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 const NavBar = () => {
-  console.log("Navbar - Rendered");
-
   return (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark-sync">
-        <div class="container-fluid">
-          {/*<a class="navbar-brand sync-type-color" href="{% url 'home' %}">
-            Sync
-            </a>*/}
-          <Link to="/">Sync</Link>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <Link
-                  class="nav-link sync-toggler-color"
-                  aria-current="page"
-                  to="/Addroom"
-                >
-                  Create
-                </Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link sync-toggler-color" to="/">
-                  Join
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Navbar bg="dark" variant="dark" className="bg-dark-sync" fixed="top">
+        <Navbar.Brand href="/">Sync</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/Create">Create</Nav.Link>
+          <Nav.Link href="/Join">Join</Nav.Link>
+        </Nav>
 
-      <script
-        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
-        crossorigin="anonymous"
-      ></script>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            signed in as, <Link href="#login">User Name</Link>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+      <div style={{ marginTop: "50px" }}></div>
     </div>
   );
 };
