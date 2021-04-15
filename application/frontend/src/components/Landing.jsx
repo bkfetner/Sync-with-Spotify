b1a04bb018eb41b6630cca4bd4b8ec201f736779
Bryan Fetner <bfetner@mail.sfsu.edu>
2021-04-15 13:45:44 -0700

    Renamed Search.jsx to Home.jsx and fixed appropriate references

--- application/frontend/src/components/Landing.jsx
+++ application/frontend/src/components/Landing.jsx
@@ -1,36 +1,49 @@
-import React, { Component, useState, Box, Flex, ImageBackground, View, Text } from "react";
+import React, {
+  Component,
+  useState,
+  Box,
+  Flex,
+  ImageBackground,
+  View,
+  Text,
+} from "react";
 import { Form, Input, Checkbox } from "antd";
 import { Link } from "react-router-dom";
 import Axios from "axios";
-import { CardDeck, Button, Container } from "react-bootstrap"
-import Background from '../assets/bg.jpg';
+import { CardDeck, Button, Container } from "react-bootstrap";
+import Background from "../assets/bg.jpg";
 import { Header } from "antd/lib/layout/layout";
-import FAQ from './FAQ'
-import '../css/Landing.css'
+import FAQ from "./FAQ";
+import "../css/Landing.css";
 
 const Landing = () => {
   return (
     <div>
-    <figure className="position-relative">
-      <img src={Background} alt="bg" className="img-fluid" style={{ filter: 'brightness(30%)', width: '100%', height: '740px' }} />
-      <figcaption className='banner'>
-        Welcome to Sync!
-        </figcaption>
-      <figcaption className='subtext1'>
-        Share your spotify songs in one of our listening rooms!<br />
+      <figure className="position-relative">
+        <img
+          src={Background}
+          alt="bg"
+          className="img-fluid"
+          style={{ filter: "brightness(30%)", width: "100%", height: "740px" }}
+        />
+        <figcaption className="banner">Welcome to Sync!</figcaption>
+        <figcaption className="subtext1">
+          Share your spotify songs in one of our listening rooms!
+          <br />
           Listen to music and chat with friends and the community!
         </figcaption>
-      <figcaption className='landingButton'>
-        <Button
-          href="/Search"
-          class="btn btn-dark sync-button-color"
-          style={{ marginBottom: "30px" }}
-          size='lg'
-        >Log in with Spotify
+        <figcaption className="landingButton">
+          <Button
+            href="/Home"
+            class="btn btn-dark sync-button-color"
+            style={{ marginBottom: "30px" }}
+            size="lg"
+          >
+            Log in with Spotify
           </Button>
-      </figcaption>
-    </figure>
-    <FAQ />
+        </figcaption>
+      </figure>
+      <FAQ />
     </div>
   );
 };
