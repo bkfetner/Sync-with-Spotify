d18337269132aebc65893aac3a7254add91df3a9
mdevillar0 <mdevillar@mail.sfsu.edu>
2021-04-15 00:04:41 -0700

    landing page update, added FAQ template

--- application/frontend/src/components/Landing.jsx
+++ application/frontend/src/components/Landing.jsx
@@ -1,12 +1,36 @@
-import React, { Component, useState, Box, Flex } from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import React, { Component, useState, Box, Flex, ImageBackground, View, Text } from "react";
+import { Form, Input, Checkbox } from "antd";
 import { Link } from "react-router-dom";
 import Axios from "axios";
+import { CardDeck, Button, Container } from "react-bootstrap"
+import Background from '../assets/bg.jpg';
+import { Header } from "antd/lib/layout/layout";
+import FAQ from './FAQ'
+import '../css/Landing.css'
 
 const Landing = () => {
   return (
     <div>
-      <Link to="/Search">Search</Link>
+    <figure className="position-relative">
+      <img src={Background} alt="bg" className="img-fluid" style={{ filter: 'brightness(30%)', width: '100%', height: '740px' }} />
+      <figcaption className='banner'>
+        Welcome to Sync!
+        </figcaption>
+      <figcaption className='subtext1'>
+        Share your spotify songs in one of our listening rooms!<br />
+          Listen to music and chat with friends and the community!
+        </figcaption>
+      <figcaption className='landingButton'>
+        <Button
+          href="/Search"
+          class="btn btn-dark sync-button-color"
+          style={{ marginBottom: "30px" }}
+          size='lg'
+        >Log in with Spotify
+          </Button>
+      </figcaption>
+    </figure>
+    <FAQ />
     </div>
   );
 };
