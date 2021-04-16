ded14e3fb68d29b20ed0c64154452c642830309c
Bryan Fetner <bfetner@mail.sfsu.edu>
2021-04-15 18:10:08 -0700

    Changed home page, needs more work. Added logo to nav bar.

--- application/frontend/src/components/Home.jsx
+++ application/frontend/src/components/Home.jsx
@@ -2,10 +2,36 @@ import React, { Component, useEffect, useState } from "react";
 import { Form, Input, Button, Checkbox } from "antd";
 import { Link } from "react-router-dom";
 import Axios from "axios";
+import "../css/Home.css";
+import Animate from "rc-animate";
 
 const Home = () => {
+  const textSets = [
+    {
+      main: "Visit a Listening Room",
+      one: "Look for a room to join.",
+      two: "Create a new room to start fresh.",
+      three: "Listen to music together.",
+    },
+    {
+      main: "Create a New Room",
+      one: "Name to your specification.",
+      two: "Choose the genre you want to feature.",
+      three: "Add songs you want to share and invite others to join.",
+    },
+    {
+      main: "Join a Room",
+      one: "Look through our recommended rooms.",
+      two:
+        "Search for a room based on roomname or genre to find on the suits your interests.",
+      three: "Use a shared room code to join your friends room.",
+    },
+  ];
+
   const [roomName, setRoomName] = useState();
   const [roomGenre, setGenre] = useState();
+
+  const [mainText, setMainText] = useState(textSets[0]);
 
   const insertData = () => {
     var data = {
@@ -23,8 +49,52 @@ const Home = () => {
 
   return (
     <div>
-      <div class="main">
-        <h1 style={{ marginTop: "30px", marginBottom: "30px" }}>
+      <div className="main home-main">
+        <div className="text-and-button-column">
+          <div className="text-only">
+            <div className="top-text">{mainText.main}</div>
+            <ul>
+              <li>{mainText.one}</li>
+              <li>{mainText.two}</li>
+              <li>{mainText.three}</li>
+            </ul>
+          </div>
+
+          <div className="button-column">
+            <Link
+              to="/Join"
+              class="btn btn-dark sync-button-color home-button-design"
+              style={{ marginBottom: "30px" }}
+              onMouseOver={() => {
+                console.log("join-over");
+                setMainText(textSets[1]);
+              }}
+              onMouseOut={() => {
+                console.log("join-out");
+                setMainText(textSets[0]);
+              }}
+            >
+              Join
+            </Link>
+            <Link
+              to="/Create"
+              class="btn btn-dark sync-button-color home-button-design"
+              style={{ marginBottom: "30px" }}
+              onMouseOver={() => {
+                console.log("create-over");
+                setMainText(textSets[2]);
+              }}
+              onMouseOut={() => {
+                console.log("create-out");
+                setMainText(textSets[0]);
+              }}
+            >
+              Create
+            </Link>
+          </div>
+        </div>
+
+        {/*<h1 style={{ marginTop: "30px", marginBottom: "30px" }}>
           Welcome to Sync!
         </h1>
         <h4>Share your spotify songs in one of our listening rooms!</h4>
@@ -43,7 +113,7 @@ const Home = () => {
 
         <h4>Search for a room to join</h4>
 
-        {/*<div class="search_box">
+        <div class="search_box">
           <div class="dropdown" style="float: left">
             <button
               class="btn btn-secondary dropdown-toggle sync-button-color"
