2f447588b54e49424105f719395dd43c6e180b0d
luongdang0701 <luongdang0701@gmail.com>
2021-04-17 21:58:43 -0700

    Room now displays Song's title and cover

--- application/frontend/src/components/Room.jsx
+++ application/frontend/src/components/Room.jsx
@@ -11,14 +11,127 @@ import "../css/Room.css";
   */
 }
 
+const albumList = [
+  {
+    title:"Pick Up Your Feelings",
+    url:"./assets/1.PNG"
+  },
+  {
+    title:"Hunger",
+    url:"./assets/2.PNG"
+  },
+  {
+    title:"no love",
+    url:"./assets/3.PNG"
+  },
+  {
+    title:"Killuminati",
+    url:"./assets/4.PNG"
+  },
+
+  
+
+
+
+
+
+  {
+    title:"no,no",
+    url:"./assets/5.PNG"
+  },
+  {
+    title:"Crime Pays",
+    url:"./assets/6.jpg"
+  },
+  {
+    title:"Ninety",
+    url:"./assets/7.jpg"
+  },
+
+
+
+
+
+  {
+    title:"Souldfood",
+    url:"./assets/8.jpg"
+  },
+  {
+    title:"Violent Crimes",
+    url:"./assets/9.jpg"
+  },
+  {
+    title:"Been Waiting!",
+    url:"./assets/10.jpg"
+  },
+
+
+
+
+
+  {
+    title:"Leray",
+    url:"./assets/11.jpg"
+  },
+  {
+    title:"HONEST",
+    url:"./assets/12.jpg"
+  },
+  {
+    title:"WOLF",
+    url:"./assets/13.jpg"
+  },
+
+
+
+
+
+  {
+    title:"Trying",
+    url:"./assets/14.jpg"
+  },
+  {
+    title:"A Calabasas Freestyle",
+    url:"./assets/15.jpg"
+  },
+  {
+    title:"Father Stretch My Hands",
+    url:"./assets/16.jpg"
+  },
+
+
+
+
+  {
+    title:"Frank's Track",
+    url:"./assets/17.jpg"
+  },
+  {
+    title:"No More Parties In LA",
+    url:"./assets/18.jpg"
+  },
+  {
+    title:"Champion",
+    url:"./assets/19.png"
+  },
+  {
+    title:"Once Upon A Time(Freestyle)",
+    url:"./assets/20.PNG"
+  }
+]
+let song1 = albumList[Math.floor(Math.random()*19)];
+let song2 = albumList[Math.floor(Math.random()*19)];
+let song3 = albumList[Math.floor(Math.random()*19)];
+let song4 = albumList[Math.floor(Math.random()*19)];
+
 const Room = (props) => {
   const [songs, setSongs] = useState({
     song_id: "1",
-    songName: "Song 1",
+    songName: song1.title,
     artist: "unknown",
     songUrl:
       "https://www.freesound.org/data/previews/338/338825_1648170-lq.mp3",
-    songImageUrl: "./assets/image0.png",
+    songImageUrl: song1.url,
   });
 
   const [currentSong, setCurrentSong] = useState(0);
@@ -38,7 +151,7 @@ const Room = (props) => {
             </div>
             <div class="songdiv">
               {/* <img src={albumCover} /> */}
-              Song 1
+              Song 1: {song1.title}
               <input
                 type="checkbox"
                 style={{
@@ -50,7 +163,7 @@ const Room = (props) => {
             </div>
             <div class="songdiv">
               {/* <img src={albumCover} /> */}
-              Song 2
+              Song 2: {song2.title}
               <input
                 type="checkbox"
                 style={{
@@ -62,7 +175,7 @@ const Room = (props) => {
             </div>
             <div class="songdiv">
               {/* <img src={albumCover} /> */}
-              Song 3
+              Song 3: {song3.title}
               <input
                 type="checkbox"
                 style={{
@@ -74,7 +187,7 @@ const Room = (props) => {
             </div>
             <div class="songdiv">
               {/* <img src={albumCover} /> */}
-              Song 4
+              Song 4: {song4.title}
               <input
                 type="checkbox"
                 style={{
