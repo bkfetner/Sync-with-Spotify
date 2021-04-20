a7802d864a79eec0780f9327c03e86606a2d8472
Hirva Patel <p.hirva98@gmail.com>
2021-04-20 12:51:07 +0530

    added dropdown to choose from roomname and genre

--- application/frontend/src/components/Join.jsx
+++ application/frontend/src/components/Join.jsx
@@ -1,8 +1,10 @@
 import React, { Component, Fragment, useEffect, useState } from "react";
-import { Form, Input, Button, Checkbox, Row, Col,Card, Typography, AutoComplete, Divider } from "antd";
+import { Form, Input, Button, Checkbox, Row, Col,Card, Typography, AutoComplete, Divider, Dropdown, Menu, message } from "antd";
 import Axios from "axios";
-import {SearchOutlined} from '@ant-design/icons'
+import { DownOutlined } from '@ant-design/icons';
 import "../css/Join.css";
+
+
 
 
 const Join = () => {
@@ -14,15 +16,28 @@ const Join = () => {
   const viewRooms = () => {
     
   };
+  const onClickfunction = ({ key }) => {
+    
+    
+    if (`${key}` == 2) {
+        searchByName();
+    }
+    else if (`${key}` == 3) {
+        searchByGenre();
+    }
+  };
+  
+  const menu = (
+    <Menu onClick={onClickfunction}>
+      
+        <Menu.Item key="2">RoomName</Menu.Item>
+        <Menu.Item key="3">Genre</Menu.Item>
+    </Menu>
+);
   useEffect(() =>{
       
       Axios.get("http://localhost:8000/api/adds/")
       .then((res) => {
-        let tempOptions = []
-        res.data.forEach(d => {       
-            tempOptions.push({value: d.room_name})
-        });
-        setOptions(tempOptions)
         setViewData(res.data)
       })
       .catch((er) => console.log(er));
@@ -34,6 +49,33 @@ const Join = () => {
     }
     }, [searchValue])
 
+  const searchAll = ()=>{
+    
+  }
+  const searchByName = () => {
+    Axios.get("http://localhost:8000/api/adds/")
+      .then((res) => {
+        let tempOptions = []
+        res.data.forEach(d => {       
+            tempOptions.push({value: d.room_name})
+        });
+        setOptions(tempOptions)
+        setViewData(res.data)
+      })
+      .catch((er) => console.log(er));
+  }
+  const searchByGenre = () => {
+    Axios.get("http://localhost:8000/api/adds/")
+      .then((res) => {
+        let tempOptions = []
+        res.data.forEach(d => {       
+            tempOptions.push({value: d.genre})
+        });
+        setOptions(tempOptions)
+        setViewData(res.data)
+      })
+      .catch((er) => console.log(er));
+  }
   const searchRoom = () => {
         if(searchValue === '') return;
         let result = viewData.filter(d => d.room_name.toLowerCase().includes(searchValue.toLowerCase()))
@@ -54,23 +96,34 @@ const Join = () => {
                     {options &&
                         <Row gutter={16}>
                             <Col xs={20} md={6}>
+                                <Dropdown 
+                                overlay={menu} 
+                                overlayClassName ="join_dropdown"
+                                >
+                                    <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                                    Search <DownOutlined />
+                                    </a>
+                                </Dropdown>
                                 <AutoComplete
-                                    style={{width: "100%",marginLeft: "580px"}} 
+                                    style={{width: "100%"}} 
                                     onSearch={(value) => {
                                         setSearchValue(value)
                                         console.log(value)
-                                        let result = viewData.filter(d => d.room_name.toLowerCase().includes(value.toLowerCase()))
+                                        let result = viewData.filter(d => d.room_name.toLowerCase().includes(value.toLowerCase()) || d.genre.toLowerCase().includes(value.toLowerCase()))
                                         setSearchedData(result)
+                                        
                                     }}
                                     onSelect={(value) => {
                                         setSearchValue(value)
                                         console.log(value)
-                                        let result = viewData.filter(d => d.room_name.toLowerCase().includes(value.toLowerCase()))
+                                        let result = viewData.filter(d => d.room_name.toLowerCase().includes(value.toLowerCase()) || d.genre.toLowerCase().includes(value.toLowerCase()))
+
                                         setSearchedData(result)
+                                        
                                     }}                        
-                                    notFoundContent="Sorry, the room with this name was not found..."          
+                                    notFoundContent="Sorry, the room with this name or genre was not found..."          
                                     options={options}
-                                    placeholder={"search rooms by \"name\""}
+                                    placeholder={"search rooms by \"name\" or \"genre\""}
                                     filterOption={(inputValue, option) =>
                                     option.value.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
                                     }
@@ -88,7 +141,7 @@ const Join = () => {
                 <Col xs={24}>
                 <div class="main">
                   <h4 style={{ marginTop: "30px", marginBottom: "30px" }}>
-                  {"Searched Rooms by name \"" + searchValue + "\""}
+                  {"Searched Rooms by  \"" + searchValue + "\""}
                   </h4>
                 </div>
 
