b855fbf5c03396431d88832b7387fe7a833e9adf
Bryan Fetner <bfetner@mail.sfsu.edu>
2021-04-21 13:51:49 -0700

    Fixed Room to accomendate for new url attribute. Fixed footer posiitoning on Createpage.

--- application/frontend/src/components/Createpage.jsx
+++ application/frontend/src/components/Createpage.jsx
@@ -1,6 +1,15 @@
 import React, { Component, useState, setState } from "react";
 import "antd/dist/antd.css";
-import { Form, Input, Button, Checkbox, Modal, message, Select, Row } from "antd";
+import {
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Modal,
+  message,
+  Select,
+  Row,
+} from "antd";
 import Axios from "axios";
 import "../css/Create.css";
 import Room from "./Room";
@@ -11,14 +20,10 @@ import Footer from "./Footer";
 const Createpage = (props) => {
   return (
     <div className="createpage-flex">
-      
       <div className="createpage-top-text-block">Create your own room!</div>
-      <div>
+      <div style={{ height: "calc(100vh - 424px)", minHeight: "500px" }}>
         <Create />
-      
       </div>
-      
-     
       <Footer />
     </div>
   );
