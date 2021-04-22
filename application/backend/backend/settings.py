d45f5cbde40625b158bd1a88f3a0b9f9a83a9e6d
Vishakha Tyagi <vishakhatyagi109@gmail.com>
2021-04-21 23:41:27 -0700

    Fixing head

--- application/backend/backend/settings.py
+++ application/backend/backend/settings.py
@@ -80,24 +80,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'djangosearchdb',
-#         'USER': 'admin',
-#         'PASSWORD': '12345678',
-#         'HOST': 'sync-db.cldl9j2fxicq.us-east-2.rds.amazonaws.com',
-#         'PORT': '3306',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sync_db',
-        'USER': 'root',
-        'PASSWORD': 'Woodside@123',
-        'HOST': 'localhost',
+        'NAME': 'djangosearchdb',
+        'USER': 'admin',
+        'PASSWORD': '12345678',
+        'HOST': 'sync-db.cldl9j2fxicq.us-east-2.rds.amazonaws.com',
         'PORT': '3306',
     }
 }
