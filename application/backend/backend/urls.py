c46c8469e43b5611e3fcc27f05618c7eef6046f3
Vishakha Tyagi <vishakhatyagi109@gmail.com>
2021-04-21 23:03:17 -0700

    Adding the back end code for User login and authentication using JWT authorisation

--- application/backend/backend/urls.py
+++ application/backend/backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 from todo import views
 
@@ -25,6 +25,8 @@ router.register(r'adds', views.RoomView, 'todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include(router.urls)),
+    path('api/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 
 ]
