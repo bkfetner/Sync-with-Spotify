fbf3e49060118dcdc12fe9fb11ebd6f066407d17
Vishakha Tyagi <vishakhatyagi109@gmail.com>
2021-05-05 08:48:23 -0700

    Adding new tables

--- application/backend/todo/models.py
+++ application/backend/todo/models.py
@@ -11,8 +11,13 @@ class Todo(models.Model):
     def _str_(self):
         return self.title
 
+class Users(models.Model):
+    display_name = models.CharField(max_length=255)
+    profile_pic = models.CharField(max_length=1000)
+    user_id = models.CharField(max_length=255)
+
 class Rooms(models.Model):
- #   room_id = models.CharField(max_length=20)
+ #  room_id = models.CharField(max_length=20)
     room_name = models.CharField(max_length=255)
     genre = models.CharField(max_length=255)
     roomImageUrl = models.CharField(max_length=255,null=True)
@@ -20,14 +25,29 @@ class Rooms(models.Model):
     def _str_(self):
         return self.room_name
 
-class Vote(models.Model):
-    user_id = models.CharField(max_length=255) #turn this into foreign key when we create user table
-    count = models.CharField(max_length=255)
-    room_id = models.ForeignKey(Rooms, on_delete=models.CASCADE) 
+class Room_list(models.Model):
+    room_name = models.CharField(Rooms, on_delete=models.CASCADE)
+    room_list_title = models.CharField(max_length=255)
+
+class Vote(models.Model):                      
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE) # number of user_id gives us the count
+    vote_id = models.CharField(max_length=255)
+    room_id = models.ForeignKey(Rooms, on_delete=models.CASCADE)
 
 class Participants(models.Model):
     room_id = models.ForeignKey(Rooms, on_delete=models.CASCADE)
     user_id = models.CharField(max_length=255)
+
+class Queue(models.Model):
+    room_id = models.ForeignKey(Rooms, on_delete=models.CASCADE)
+    queue_id = models.CharField(max_length=255)
+    song_list_id = models.IntegerField()
+    queue_history = models.CharField(max_length=255)
+
+  
+
+    
+  
 
 # class UserAccountManager(BaseUserManager):
 #     def create_user(self, email, password=None, **extra_fields):
