08bdd057ff9fa8e27b8fabd31fcbd7eaa5ef4957
Bryan Fetner <bfetner@mail.sfsu.edu>
2021-05-15 16:25:43 -0700

    Updated chatroom design to match rest of app.

--- application/frontend/src/components/Chatroom.jsx
+++ application/frontend/src/components/Chatroom.jsx
@@ -1,47 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { w3cwebsocket as W3CWebSocket } from "websocket";
 
 import "../css/Chat.css";
 
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import Paper from '@material-ui/core/Paper';
-import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import Paper from "@material-ui/core/Paper";
+import Avatar from "@material-ui/core/Avatar";
+import { Button, Input } from "antd";
 
 import { withStyles } from "@material-ui/core/styles";
 
-const useStyles = theme => ({
+const useStyles = (theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
   root: {
-    boxShadow: 'none',
-  }
+    boxShadow: "none",
+  },
 });
 
 class Chatroom extends Component {
-
   constructor(props) {
     super(props);
   }
@@ -49,33 +48,33 @@ class Chatroom extends Component {
   state = {
     isLoggedIn: true,
     messages: [],
-    value: '',
+    value: "",
     name: this.props.displayName,
     room: this.props.roomName,
-  }
+  };
 
-
-
-  client = new W3CWebSocket('ws://localhost:8000/ws/chat/' + this.props.roomId + '/'); /* put id here */
+  client = new W3CWebSocket(
+    "ws://localhost:8000/ws/chat/" + this.props.roomId + "/"
+  ); /* put id here */
 
   onButtonClicked = (e) => {
     var sendStr = JSON.stringify({
       type: "message",
       message: this.state.value,
       name: this.props.displayName,
-      profileUrl: this.props.profilePictureUrl
+      profileUrl: this.props.profilePictureUrl,
     });
     console.log("sendStr");
     console.log(sendStr);
 
     this.client.send(sendStr);
-    this.state.value = ''
+    this.state.value = "";
     e.preventDefault();
-  }
+  };
 
   componentDidMount() {
     this.client.onopen = () => {
-      console.log('WebSocket Client Connected');
+      console.log("WebSocket Client Connected");
     };
     this.client.onmessage = (message) => {
       console.log("message");
@@ -86,78 +85,110 @@ class Chatroom extends Component {
       console.log("dataFromServer.name");
       console.log(dataFromServer.name);
 
-      console.log('got reply! ', dataFromServer.type);
+      console.log("got reply! ", dataFromServer.type);
       if (dataFromServer) {
-        this.setState((state) =>
-          ({
-            messages: [...state.messages,
+        this.setState((state) => ({
+          messages: [
+            ...state.messages,
             {
               msg: dataFromServer.message,
               name: dataFromServer.name,
-              profileUrl: dataFromServer.profileUrl
-            }]
-          })
-        );
+              profileUrl: dataFromServer.profileUrl,
+            },
+          ],
+        }));
       }
     };
   }
 
   render() {
-
     const { classes } = this.props;
     return (
-      <Container component="main" maxWidth="xs">
-        {this.state.isLoggedIn ?
-          <div style={{ marginTop: 50, }}>
-            Room Name: {this.state.room}
-            <Paper style={{ height: 500, maxHeight: 500, overflow: 'auto', boxShadow: 'none', flexDirection: 'column-reverse'}} className="realtime-chat-messages">
-              {this.state.messages.reverse().map(message => <>
-                <Card className={classes.root} style={{ minHeight: "80px"}}>
-                  <CardHeader
-                    avatar={
-                      <Avatar className={classes.avatar} src={message.profileUrl}>
+      <Container style={{height: "calc(100% - 108px)", padding: "15px"}}>
+        {this.state.isLoggedIn ? (
+          <div className="realtime-containter">
+            <div className="chat-top-text">The {this.state.room} Chat Room</div>
+            <Paper className="realtime-chat-messages">
+              {this.state.messages.reverse().map((message) => (
+                <>
+                  <div>
+                    <div className="realtime-card-flex">
+                      <Avatar
+                        className={classes.avatar}
+                        className="realtime-avatar"
+                        src={message.profileUrl}
+                      >
                         {message.name.charAt(0)}
-                  </Avatar>
-                    }
-                    title={message.name}
-                    subheader={message.msg}
-                  />
-                </Card>
-              </>)}
+                      </Avatar>
+                      <div>
+                        <div className="realtime-card-title">
+                          {message.name}
+                        </div>
+                        <div className="realtime-card-subheader">
+                          {message.msg}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ))}
             </Paper>
-
-            <form className={classes.form} noValidate onSubmit={this.onButtonClicked}>
-              <TextField
+            <form
+              className={classes.form}
+              noValidate
+              onSubmit={this.onButtonClicked}
+            >
+              <Input
+                placeholder="Send a Message"
+                onChange={(e) => {
+                  this.setState({ value: e.target.value });
+                  this.value = this.state.value;
+                }}
+                className="chat-input"
+                autoFocus
+                autoComplete="off"
+                allowClear="true"
+                value={this.state.value}
+              />
+              {/* <TextField
                 id="outlined-helperText"
                 label="Make a comment"
                 defaultValue="Default Value"
                 variant="outlined"
                 value={this.state.value}
                 fullWidth
-                onChange={e => {
+                onChange={(e) => {
                   this.setState({ value: e.target.value });
                   this.value = this.state.value;
                 }}
-              />
+                style={{
+                  backgroundColor: "var(--color1)",
+                  borderRadius: "5px",
+                  color: "var(--color4)",
+                }}
+              /> */}
               <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
+                type="primary"
+                htmlType="submit"
+                className="sync-button-color"
+                style={{ marginTop: "10px" }}
               >
-                Start Chatting
-                </Button>
+                Send Comment
+              </Button>
             </form>
           </div>
-          :
+        ) : (
           <div>
             <CssBaseline />
             <div className={classes.paper}>
               <Typography component="h1" variant="h5">
                 ChattyRooms
-                </Typography>
-              <form className={classes.form} noValidate onSubmit={value => this.setState({ isLoggedIn: true })}>
+              </Typography>
+              <form
+                className={classes.form}
+                noValidate
+                onSubmit={(value) => this.setState({ isLoggedIn: true })}
+              >
                 <TextField
                   variant="outlined"
                   margin="normal"
@@ -168,7 +199,7 @@ class Chatroom extends Component {
                   name="Chatroom Name"
                   autoFocus
                   value={this.state.room}
-                  onChange={e => {
+                  onChange={(e) => {
                     this.setState({ room: e.target.value });
                     this.value = this.state.room;
                   }}
@@ -183,7 +214,7 @@ class Chatroom extends Component {
                   type="Username"
                   id="Username"
                   value={this.state.name}
-                  onChange={e => {
+                  onChange={(e) => {
                     this.setState({ name: e.target.value });
                     this.value = this.state.name;
                   }}
@@ -196,12 +227,12 @@ class Chatroom extends Component {
                   className={classes.submit}
                 >
                   Start Chatting
-                  </Button>
+                </Button>
                 <Grid container>
                   <Grid item xs>
                     <Link href="#" variant="body2">
                       Forgot password?
-                      </Link>
+                    </Link>
                   </Grid>
                   <Grid item>
                     <Link href="#" variant="body2">
@@ -211,10 +242,10 @@ class Chatroom extends Component {
                 </Grid>
               </form>
             </div>
-          </div>}
+          </div>
+        )}
       </Container>
-    )
-
+    );
   }
 }
-export default withStyles(useStyles)(Chatroom)
+export default withStyles(useStyles)(Chatroom);
