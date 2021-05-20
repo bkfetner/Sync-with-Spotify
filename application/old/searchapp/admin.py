da77fe2beb5f1c738d4bb157aecf3b11b606d3a2
Bryan Fetner <bfetner@mail.sfsu.edu>
2021-05-19 22:48:21 -0700

    Prep for merge

--- application/old/searchapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Rooms
-
-class RoomAdmin(admin.ModelAdmin):
-    list_display = ("room_id", "room_name", "genre")
-
-admin.site.register(Rooms, RoomAdmin)
